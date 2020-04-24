92a515eab88ac070da54dab7ac3dc9b5145a2054
Davisdavy <davisdavy96@gmail.com>
2020-04-25 02:26:27 +0300

    Create DAO and database

---
+++ app/src/main/java/com/davis/mvvm/Note.java
@@ -0,0 +1,45 @@
+package com.davis.mvvm;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private  String description;
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+
+    public int getPriority() {
+        return priority;
+    }
+
+
+}
