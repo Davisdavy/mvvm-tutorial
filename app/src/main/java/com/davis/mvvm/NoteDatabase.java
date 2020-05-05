56a322c2bbfb20221202047eb6d95c6e67485a9d
Davisdavy <davisdavy96@gmail.com>
2020-05-06 01:08:48 +0300

    show data oncreate

--- app/src/main/java/com/davis/mvvm/NoteDatabase.java
+++ app/src/main/java/com/davis/mvvm/NoteDatabase.java
@@ -2,10 +2,13 @@ package com.davis.mvvm;
 
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Note.class, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -18,9 +21,36 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)//atach callback
                     .build();
         }
         return instance;
+    }
+
+    //Show data on create
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            //execute async class in the on create
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+    //Show data on create in async task
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
+        private NoteDao noteDao;
+        //happens when the database is created
+        public PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 2));
+            noteDao.insert(new Note("Title 3", "Description 3", 3));
+            return null;
+        }
     }
 
 }
