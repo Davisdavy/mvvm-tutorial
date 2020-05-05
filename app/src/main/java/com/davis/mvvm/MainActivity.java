f362c10e03f80cae263d35a9bb70cc83cba3082a
Davisdavy <davisdavy96@gmail.com>
2020-05-06 01:49:36 +0300

    Reference viewmodel to the view

--- app/src/main/java/com/davis/mvvm/MainActivity.java
+++ app/src/main/java/com/davis/mvvm/MainActivity.java
@@ -1,14 +1,29 @@
 package com.davis.mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private NoteViewModel noteViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update our recyclerview
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
